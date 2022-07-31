ec523ac9fbe6af1cc45450315606bb43252ae817
Simone Boccuzzi <mughetsu2@gmail.com>
2022-07-31 18:55:26 +0200

    added page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        sm: '480px'
+      },
       colors: {
         primary: '#ff6363',
         secondary: {
           '100': '#e2e2d5',
           '200': '#888883'
-        } 
+        }
+      },
+      gridTemplateColumns: {
+        'auto-fill-100': 'repeat(auto-fill, minmax(100px, 1fr))',
+        'auto-fill-200': 'repeat(auto-fill, minmax(200px, 1fr))',
+        'auto-fit-100': 'repeat(auto-fit, minmax(100px, 1fr))',
+        'auto-fit-200': 'repeat(auto-fit, minmax(200px, 1fr))',
       },
     },
     fontFamily: {
